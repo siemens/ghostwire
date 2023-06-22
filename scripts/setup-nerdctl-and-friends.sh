e69b69a504d3b9a7b78865c45f13f4c3fabf5553
thediveo <thediveo@gmx.eu>
2023-06-22 17:03:17 +0200

    ci: work around nerdctl issue 2321

--- scripts/setup-nerdctl-and-friends.sh
+++ scripts/setup-nerdctl-and-friends.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+set -e
 
 # Download aux tools (nerdctl, CNI plugins) required to run some of the Gostwire
 # tests.
 #
-# On purpose, we're not going to install from often totally outdated or lacking
+# On purpose, we're not going to install from often totally outdated or "sparse"
 # distro repositories, but from the Github projects directly.
 
 CNI_PLUGINS_VERSION=v1.3.0
 NERDCTL_VERSION=v1.4.0
+
+WGET_PREX="/tmp/nerdctl-and-friends"
 
 BINDIR=${BINDIR:-/usr/local/bin}
 CNIBINDIR=${CNIBINDIR:-/opt/cni/bin}
@@ -15,10 +18,11 @@ CNIBINDIR=${CNIBINDIR:-/opt/cni/bin}
 ARCH=$(dpkg --print-architecture)
 echo "downloading tools for architecture ${ARCH}"
 
-WGET_PREX="/tmp/nerdctl-and-friends"
 mkdir -p ${WGET_PREX}
-wget -P ${WGET_PREX} --backups=1 https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz
-wget -P ${WGET_PREX} --backups=1 https://github.com/containerd/nerdctl/releases/download/${NERDCTL_VERSION}/nerdctl-${NERDCTL_VERSION#v}-linux-${ARCH}.tar.gz
+trap 'rm -rf -- "${WGET_PREX}"' EXIT
+
+wget -q -P ${WGET_PREX} --backups=1 https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz
+wget -q -P ${WGET_PREX} --backups=1 https://github.com/containerd/nerdctl/releases/download/${NERDCTL_VERSION}/nerdctl-${NERDCTL_VERSION#v}-linux-${ARCH}.tar.gz
 
 echo "installing CNI plugins into ${CNIBINDIR}"
 mkdir -p ${CNIBINDIR}
