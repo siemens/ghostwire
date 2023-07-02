19f2e7aee5b550e58e39a1f55a5e7ac087f9eab9
thediveo <thediveo@gmx.eu>
2023-07-02 17:52:41 +0200

    fix: lock M0 in package test
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- network/package_test.go
+++ network/package_test.go
@@ -5,11 +5,18 @@
 package network
 
 import (
+	"runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
+
+func init() {
+	// avoid M0 ending up wedged as it was used during a throw-away namespace
+	// switch, but as M0 is special it cannot be killed.
+	runtime.LockOSThread()
+}
 
 func TestGostwireNetwork(t *testing.T) {
 	RegisterFailHandler(Fail)
