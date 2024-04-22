765f977c42f1d7cebfe0eabaadbab45f65600aa7
thediveo <thediveo@gmx.eu>
2024-04-22 13:55:59 +0200

    test: more kube-proxy tests
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- network/portfwd/kubeproxy/kubeproxy_test.go
+++ network/portfwd/kubeproxy/kubeproxy_test.go
@@ -7,8 +7,11 @@ package kubeproxy
 import (
 	"net"
 
+	"github.com/google/nftables"
 	"github.com/google/nftables/expr"
 	"github.com/google/nftables/xt"
+	"github.com/thediveo/nufftables"
+	"github.com/thediveo/nufftables/portfinder"
 	"golang.org/x/sys/unix"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -16,6 +19,248 @@ import (
 )
 
 var _ = Describe("kube-proxy port forwarding", func() {
+
+	Context("detecting forwarded kube-proxy ports", func() {
+
+		It("doesn't crash", func() {
+			Expect(PortForwardings(nufftables.TableMap{}, nufftables.TableFamilyINet))
+			Expect(PortForwardings(nil, nufftables.TableFamilyIPv4))
+			Expect(PortForwardings(nufftables.TableMap{
+				nufftables.TableKey{Name: "nat", Family: nufftables.TableFamilyIPv4}: &nufftables.Table{},
+			}, nufftables.TableFamilyIPv4))
+			Expect(PortForwardings(nufftables.TableMap{
+				nufftables.TableKey{Name: "nat", Family: nufftables.TableFamilyIPv4}: &nufftables.Table{
+					ChainsByName: map[string]*nufftables.Chain{
+						kubeServicesChain: {
+							Rules: []nufftables.Rule{
+								{
+									Rule: &nftables.Rule{
+										Exprs: nil,
+									},
+								},
+							},
+						},
+					},
+				},
+			}, nufftables.TableFamilyIPv4))
+		})
+
+		It("detects forwarded ports", func() {
+			comment := xt.Comment("foo")
+			tables := nufftables.TableMap{
+				{Name: "nat", Family: nufftables.TableFamilyIPv4}: &nufftables.Table{
+					ChainsByName: map[string]*nufftables.Chain{
+						kubeServicesChain: {
+							Rules: []nufftables.Rule{
+								{
+									Rule: &nftables.Rule{
+										Exprs: []expr.Any{
+											&expr.Cmp{
+												Data: []byte{unix.IPPROTO_TCP},
+											},
+											&expr.Cmp{
+												Data: net.ParseIP("1.2.3.4"),
+											},
+											&expr.Match{
+												Name: "comment",
+												Info: &comment,
+											},
+											&expr.Cmp{
+												Data: []byte{1, 2},
+											},
+											&expr.Verdict{
+												Kind:  expr.VerdictJump,
+												Chain: kubeServiceChainPrefix + "HELLORLD",
+											},
+										},
+									},
+								},
+							},
+						},
+						kubeServiceChainPrefix + "HELLORLD": {
+							Rules: []nufftables.Rule{
+								{
+									Rule: &nftables.Rule{
+										Exprs: []expr.Any{
+											&expr.Verdict{
+												Kind:  expr.VerdictJump,
+												Chain: kubeSeparationChainPrefix + "FOO",
+											},
+										},
+									},
+								},
+								{
+									Rule: &nftables.Rule{
+										Exprs: []expr.Any{
+											&expr.Verdict{
+												Kind:  expr.VerdictJump,
+												Chain: kubeSeparationChainPrefix + "BAR",
+											},
+										},
+									},
+								},
+								{
+									Rule: &nftables.Rule{
+										Exprs: []expr.Any{
+											&expr.Verdict{
+												Kind:  expr.VerdictJump,
+												Chain: kubeSeparationChainPrefix + "BAZ",
+											},
+										},
+									},
+								},
+								{
+									Rule: &nftables.Rule{
+										Exprs: []expr.Any{
+											&expr.Verdict{
+												Kind:  expr.VerdictJump,
+												Chain: kubeSeparationChainPrefix + "BAZZZZ",
+											},
+										},
+									},
+								},
+							},
+						},
+						kubeSeparationChainPrefix + "FOO": {
+							Rules: []nufftables.Rule{
+								{
+									Rule: &nftables.Rule{
+										Exprs: []expr.Any{
+											&expr.Target{
+												Name: "DNAT",
+												Info: &xt.NatRange2{
+													NatRange: xt.NatRange{
+														MinIP:   net.ParseIP("10.20.30.40"),
+														MinPort: 123,
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						kubeSeparationChainPrefix + "BAR": {
+							Rules: []nufftables.Rule{
+								{
+									Rule: &nftables.Rule{
+										Exprs: []expr.Any{
+											&expr.Target{
+												Name: "DNAT",
+												Info: &xt.NatRange2{
+													NatRange: xt.NatRange{
+														MinIP:   net.ParseIP("10.20.30.44"),
+														MinPort: 123,
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						kubeSeparationChainPrefix + "BAZ": {
+							Rules: []nufftables.Rule{
+								{
+									Rule: &nftables.Rule{
+										Exprs: []expr.Any{
+											&expr.Target{},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			portfwds := PortForwardings(tables, nufftables.TableFamilyIPv4)
+			Expect(portfwds).To(ConsistOf(
+				&portfinder.ForwardedPortRange{
+					Protocol:       "tcp",
+					IP:             net.ParseIP("1.2.3.4"),
+					PortMin:        0x0102,
+					PortMax:        0x0102,
+					ForwardIP:      net.ParseIP("10.20.30.40"),
+					ForwardPortMin: 123,
+				},
+				&portfinder.ForwardedPortRange{
+					Protocol:       "tcp",
+					IP:             net.ParseIP("1.2.3.4"),
+					PortMin:        0x0102,
+					PortMax:        0x0102,
+					ForwardIP:      net.ParseIP("10.20.30.44"),
+					ForwardPortMin: 123,
+				},
+			))
+		})
+
+	})
+
+	It("matches DNAT target expressions", func() {
+		dnat, ok := getDNAT(&expr.Target{Name: "hellorld"})
+		Expect(ok).To(BeFalse())
+		Expect(dnat).To(BeNil())
+
+		dnat, ok = getDNAT(&expr.Target{Name: "DNAT"})
+		Expect(ok).To(BeFalse())
+		Expect(dnat).To(BeNil())
+
+		dnat, ok = getDNAT(&expr.Target{
+			Name: "DNAT",
+			Info: &xt.NatRange2{
+				BasePort: 42,
+			},
+		})
+		Expect(ok).To(BeTrue())
+		Expect(dnat).NotTo(BeNil())
+		Expect(dnat.BasePort).To(Equal(uint16(42)))
+	})
+
+	Context("service provider chains", func() {
+
+		It("returns nothing for non-existing chain", func() {
+			Expect(serviceProviderChains(nil)).To(BeNil())
+		})
+
+		It("returns separated service provider chains", func() {
+			chains := serviceProviderChains(&nufftables.Chain{
+				Rules: []nufftables.Rule{
+					{
+						Rule: &nftables.Rule{
+							Exprs: []expr.Any{
+								&expr.Bitwise{},
+							},
+						},
+					},
+					{
+						Rule: &nftables.Rule{
+							Exprs: []expr.Any{
+								&expr.Verdict{
+									Kind:  expr.VerdictJump,
+									Chain: kubeSeparationChainPrefix + "HELLORLD",
+								},
+							},
+						},
+					},
+					{
+						Rule: &nftables.Rule{
+							Exprs: []expr.Any{
+								&expr.Verdict{
+									Kind:  expr.VerdictJump,
+									Chain: kubeSeparationChainPrefix + "GOODBYE",
+								},
+							},
+						},
+					},
+				},
+			})
+			Expect(chains).To(ConsistOf(
+				kubeSeparationChainPrefix+"HELLORLD",
+				kubeSeparationChainPrefix+"GOODBYE",
+			))
+		})
+
+	})
 
 	Context("transport protocol", func() {
 
@@ -85,21 +330,21 @@ var _ = Describe("kube-proxy port forwarding", func() {
 
 	})
 
-	Context("jump verdicts", func() {
+	Context("service jump verdicts", func() {
 
 		It("ignores non-service jump targets other verdicts", func() {
-			chain, ok := getJumpVerdictChain(&expr.Verdict{})
+			chain, ok := getJumpVerdictServiceChain(&expr.Verdict{})
 			Expect(ok).To(BeFalse())
 			Expect(chain).To(BeEmpty())
 
-			chain, ok = getJumpVerdictChain(&expr.Verdict{
+			chain, ok = getJumpVerdictServiceChain(&expr.Verdict{
 				Kind:  expr.VerdictContinue,
 				Chain: "foobar",
 			})
 			Expect(ok).To(BeFalse())
 			Expect(chain).To(BeEmpty())
 
-			chain, ok = getJumpVerdictChain(&expr.Verdict{
+			chain, ok = getJumpVerdictServiceChain(&expr.Verdict{
 				Kind:  expr.VerdictJump,
 				Chain: "hellorld",
 			})
@@ -108,9 +353,43 @@ var _ = Describe("kube-proxy port forwarding", func() {
 		})
 
 		It("returns the service target chain name", func() {
-			chain, ok := getJumpVerdictChain(&expr.Verdict{
+			chain, ok := getJumpVerdictServiceChain(&expr.Verdict{
 				Kind:  expr.VerdictJump,
 				Chain: "KUBE-SVC-HELLORLD",
+			})
+			Expect(ok).To(BeTrue())
+			Expect(chain).NotTo(BeEmpty())
+
+		})
+
+	})
+
+	Context("separation jump verdicts", func() {
+
+		It("ignores non-separation jump targets other verdicts", func() {
+			chain, ok := getJumpVerdictSeparationChain(&expr.Verdict{})
+			Expect(ok).To(BeFalse())
+			Expect(chain).To(BeEmpty())
+
+			chain, ok = getJumpVerdictSeparationChain(&expr.Verdict{
+				Kind:  expr.VerdictContinue,
+				Chain: "foobar",
+			})
+			Expect(ok).To(BeFalse())
+			Expect(chain).To(BeEmpty())
+
+			chain, ok = getJumpVerdictSeparationChain(&expr.Verdict{
+				Kind:  expr.VerdictJump,
+				Chain: "hellorld",
+			})
+			Expect(ok).To(BeFalse())
+			Expect(chain).To(BeEmpty())
+		})
+
+		It("returns the service target chain name", func() {
+			chain, ok := getJumpVerdictSeparationChain(&expr.Verdict{
+				Kind:  expr.VerdictJump,
+				Chain: "KUBE-SEP-HELLORLD",
 			})
 			Expect(ok).To(BeTrue())
 			Expect(chain).NotTo(BeEmpty())
